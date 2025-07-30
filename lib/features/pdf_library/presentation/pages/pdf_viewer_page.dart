595bfdb73a6712bde103ad06ccee9bfefd7ba6ae
sachin khatri <sachin10392313@gmail.com>
2025-07-31 01:42:35 +0545

    Ui fix

---
+++ lib/features/pdf_library/presentation/pages/pdf_viewer_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class PdfViewerPage extends StatelessWidget {
+  final String url;
+  const PdfViewerPage({super.key, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("PDF Preview"),
+        backgroundColor: const Color(0xFF1E2B3A),
+      ),
+      body: SfPdfViewer.network(url),
+    );
+  }
+}
